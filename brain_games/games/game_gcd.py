9215d7d0b2b7f61a3909ae70262c597a74b92d6e
Nikolai Loginov <Loginton@gmail.com>
2024-01-20 01:29:43 +0300

    add game_gcd and brain_gcd

---
+++ brain_games/games/game_gcd.py
@@ -0,0 +1,17 @@
+import random
+
+
+def get_question():
+    question_text = 'Find the greatest common divisor of given numbers.'
+    first_number = random.randint(1, 100)
+    second_number = random.randint(1, 100)
+    question = f'{first_number} {second_number}'
+    correct_answer = gcd(first_number, second_number)
+    return question_text, question, correct_answer
+
+
+def gcd(num1, num2):
+    while num2:
+        num1, num2 = num2,num1 % num2
+    return num1
+    
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games.game_gcd import get_question
+from brain_games.games.engine import launch_game
+
+
+def main():
+    launch_game(get_question)
+
+
+if __name__ == '__main__':
+    main()
