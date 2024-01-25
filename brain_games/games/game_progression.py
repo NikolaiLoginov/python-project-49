0e1edcd7a90c42e476bf6d4bc7fc7e29a3599a94
Nikolai Loginov <Loginton@gmail.com>
2024-01-25 22:25:44 +0300

    fix bug in game_progression.py

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -4,13 +4,13 @@ import random
 def get_question():
     question_text = 'What number is missing in the progression?'
     progression_length = 10
-    hide_position = random.randint(1, progression_length)
+    hide_position = random.randint(0, progression_length - 1)
     start_number = random.randint(1, 20)
     step_number = random.randint(1, 5)
     question_list = [start_number]
     question = ''
     for n in range(progression_length - 1):
-        question_list.insert(n + 1, question_list[n] + step_number)
+        question_list.append(question_list[n] + step_number)
     correct_answer = question_list.pop(hide_position)
     question_list.insert(hide_position, '..')
     question = ' '.join(str(number) for number in question_list)
