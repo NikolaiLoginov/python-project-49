bbc8cd61fb3bc915b2c3e6e7455b6135b925a8f2
Nikolai Loginov <Loginton@gmail.com>
2024-01-31 00:09:23 +0300

    rework engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,30 +1,18 @@
 import prompt
 
 
-def welcome_user():
+def launch_game(get_question, question_text):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    return name
-
-
-def check_answer(user_answer, correct_answer):
-    if str(user_answer) == str(correct_answer):
-        return True
-    else:
-        return False
-
-
-def launch_game(get_question):
-    name = welcome_user()
-    print(get_question()[0])
+    print(question_text)
 
     for _ in range(3):
-        _, question, correct_answer = get_question()
+        question, correct_answer = get_question()
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
 
-        if check_answer(user_answer, correct_answer):
+        if str(user_answer) == str(correct_answer):
             print('Correct!')
         else:
             print(
--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -5,23 +5,23 @@ QUESTION_TEXT = 'What number is missing in the progression?'
 
 
 def get_question():
-    progression = make_progression()
-    question, correct_answer = make_question(progression)
+    start_number = random.randint(1, 20)
+    step_number = random.randint(1, 5)
+    progression = make_progression(start_number, step_number)
+    hide_position = random.randint(0, len(progression) - 1)
+    question, correct_answer = make_question(progression, hide_position)
     return question, correct_answer
 
 
-def make_progression():
+def make_progression(start_number, step_number):
     progression_length = 10
-    start_number = random.randint(1, 20)
-    step_number = random.randint(1, 5)
     question_list = [start_number]
     for n in range(progression_length - 1):
         question_list.append(question_list[n] + step_number)
     return question_list
 
 
-def make_question(question_list):
-    hide_position = random.randint(0, len(question_list) - 1)
+def make_question(question_list, hide_position):
     hidden_number = question_list[hide_position]
     question_list[hide_position] = '..'
     question = ' '.join(str(number) for number in question_list)
