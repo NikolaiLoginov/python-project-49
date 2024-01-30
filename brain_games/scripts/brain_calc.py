e8ad9b745b1b1328cf310dc3194c7f722e272986
Nikolai Loginov <Loginton@gmail.com>
2024-01-31 00:07:32 +0300

    rework scripts

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.game_calc import get_question
-from brain_games.games.engine import launch_game
+from brain_games.games.game_calc import get_question, QUESTION_TEXT
+from brain_games.engine import launch_game
 
 
 def main():
-    launch_game(get_question)
+    launch_game(get_question, QUESTION_TEXT)
 
 
 if __name__ == '__main__':
